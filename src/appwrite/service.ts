b1fb997feac7c17df25ad0e4bd91409307bd1ba3
Pundalik Desai <pundalik.desai@shwaira.com>
2025-12-05 20:57:42 +0530

    services.ts file completed  appwriteclient code completed

---
+++ src/appwrite/service.ts
@@ -0,0 +1,100 @@
+import { Client, Account, ID } from 'appwrite'
+import { Config } from 'react-native-config';
+import Snackbar from 'react-native-snackbar';   
+
+const appwriteClient = new Client()
+    
+const APPWRITE_ENDPOINT:  string = Config.APPWRITE_ENDPOINT!;
+const APPWRITE_PROJECT :string = Config.APPWRITE_PROJECT!;
+
+type CreateUserAccount ={
+    name: string;
+    email: string;
+    password: string;
+}
+
+type LoginUserAccount ={
+    email: string;
+    password: string;
+}
+
+class AppwriteService {
+    account;
+
+    constructor() {
+        appwriteClient
+            .setEndpoint(APPWRITE_ENDPOINT)
+            .setProject(APPWRITE_PROJECT);
+        this.account = new Account(appwriteClient);
+    }
+
+   //CREATE A NEW RECORD OF USER INSIDE APPWRITE
+    async createUserAccount({ name, email, password }: 
+        CreateUserAccount) {
+        try {
+            const userAccount = await this.account.create( ID.unique(), email, password, name);
+
+            if (userAccount) {
+                //todo:create login  feature after account creation
+                return this.login({ email, password });
+            }
+            else{
+                return userAccount;
+            }
+        } catch (error) {
+            Snackbar.show({
+                text: String(error),
+                duration: Snackbar.LENGTH_LONG,
+                backgroundColor: 'red',
+            });
+            throw error;
+            console.log( " APPWRITE SERVICE :: createUserAccount() :: " +  error);
+        }
+        }
+    //LOGIN USER
+    async login({ email, password }: LoginUserAccount) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        }
+        catch (error) {
+            Snackbar.show({
+                text: String(error),
+                duration: Snackbar.LENGTH_LONG,
+                backgroundColor: 'red',
+            });
+            throw error;
+            console.log( " APPWRITE SERVICE :: loginAccount() :: " +  error);
+        }
+    }
+   
+
+    //GET CURRENT LOGGED IN USER
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            Snackbar.show({
+                text: String(error),
+                duration: Snackbar.LENGTH_LONG,
+                backgroundColor: 'red',
+            });
+            throw error;
+            console.log( " APPWRITE SERVICE :: getCurrentAccount() :: " +  error);
+        }
+    }
+    //LOGOUT USER
+    async logoutUser() {
+        try { 
+            return await this.account.deleteSession('current');
+        } catch (error) {
+            Snackbar.show({
+                text: String(error),        
+                duration: Snackbar.LENGTH_LONG,
+                backgroundColor: 'red',
+            }); 
+            throw error;
+            console.log( " APPWRITE SERVICE :: logoutUser() :: " +  error);
+        }           
+    }
+
+export default  AppwriteService
